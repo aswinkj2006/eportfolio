cbf0fa3c1b3d3517f52e8edcf74b730f1d3b17a0
Aswin <aswinkj2006@gmail.com>
2025-05-21 08:02:56 +0530

    Made Mobile Responsive

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
   const accordions = document.querySelectorAll('.accordion-header');
 
   accordions.forEach(header => {
